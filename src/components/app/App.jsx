5a0f0368b3e3884e7cb0c7da69a4568144ac88ca
Serpash <ser7290@gmail.com>
2023-04-24 14:55:11 +0300

    adding "react-router-dom " to the project, making links to comics and characters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import {useState} from "react";
 import AppHeader from "../appHeader/AppHeader";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
@@ -18,25 +19,34 @@ const App = () => {
     }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <ErrorBoundary>
+                                <RandomChar/>
+                            </ErrorBoundary>
+                            <div className="char__content">
+                                <ErrorBoundary>
+                                    <CharList onCharSelected={onCharSelected}/>
+                                </ErrorBoundary>
+                                <ErrorBoundary>
+                                    <CharInfo charId={selectedChar}/>
+                                </ErrorBoundary>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                        </Route>
+
+                        <Route exact path="/comics">
+                            <AppBanner/>
+                            <ComicsList/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
--- src/services/useMarvelService.js
+++ src/services/useMarvelService.js
@@ -43,7 +43,6 @@ const useMarvelService = () => {
             description: comics.description || "There is no description",
             pageCount: comics.pageCount ? `${comics.pageCount} p.` : "No information about the number of pages",
             thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
-            language: comics.textObjects[0].language || "en-us",
             price: comics.prices[0].price ? `${comics.prices[0].price}$` : "not available",
         };
     };
